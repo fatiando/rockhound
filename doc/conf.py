55cf413d82450ae5639d6e075415d1b5ee6dadba
Leonardo Uieda <leouieda@gmail.com>
2019-05-08 17:41:43 -1000

    Add the age of the oceanic lithosphere grids
    Fetch and load the netCDF version of the age and uncertainty grids from
    the NOAA NCEI website. Available in the 2 and 6 arc-minute resolutions.
    Add metadata to the Dataset, like units, long names, and the Müller et
    al (2008) paper DOI.
    
    Fixes #9

--- doc/conf.py
+++ doc/conf.py
@@ -74,7 +74,7 @@ master_doc = 'index'
 
 # General information about the project
 year = datetime.date.today().year
-project = 'rockhound'
+project = 'RockHound'
 copyright = '2018-{}, The RockHound Developers'.format(year)
 if len(full_version.split('+')) > 1 or full_version == 'unknown':
     version = 'dev'
