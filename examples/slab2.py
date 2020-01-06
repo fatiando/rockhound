27bb7f822ced12e7db41284078a603188b4ee0f4
Agustina Pesce <pesce.agustina@gmail.com>
2020-01-06 16:47:21 -0300

    Plot all subduction zones in a map

--- examples/slab2.py
+++ examples/slab2.py
@@ -13,19 +13,48 @@ import matplotlib.pyplot as plt
 import cmocean
 import cartopy.crs as ccrs
 
-# Load a geometry of the Alaska subduction zone
-grid = rh.fetch_slab2(zone="south_america")
-print(grid)
-
-# Plot the sibductin zones using cartopy
 plt.figure(figsize=(10, 5))
 ax = plt.axes(projection=ccrs.Robinson())
-pc = grid.depth.plot.pcolormesh(
-    cmap=cmocean.cm.thermal_r,
-    cbar_kwargs=dict(pad=0.01, aspect=30),
-    ax=ax,
-    transform=ccrs.PlateCarree(),
-)
+
+zones = {
+    "alaska",
+    "calabria",
+    "caribbean",
+    "cascadia",
+    "central_america",
+    "cotabalo",
+    "halmahera",
+    "hellenic",
+    "himalaya",
+    "hindu_kush",
+    "izu_bonin",
+    "kamchatka",
+    "kermadec",
+    "makran",
+    "manila_trench",
+    "muertos_trough",
+    "new_guinea",
+    "pamir",
+    "philippines",
+    "puysegur",
+    "ryukyu",
+    "scotia_sea",
+    "solomon_islands",
+    "south_america",
+    "sulawesi",
+    "sumatra_java",
+    "vanuatu",
+}
+# Load a geometry of the Alaska subduction zone
+for zone in zones:
+    grid = rh.fetch_slab2(zone)
+
+    pc = grid.depth.plot.pcolormesh(
+        cmap=cmocean.cm.thermal_r,
+        ax=ax,
+        transform=ccrs.PlateCarree(),
+        add_colorbar=False
+    )
 # make the map global rather than have it zoom in to the extents of any plotted data
 ax.set_global()
 ax.coastlines()
